4c431dd9cd951fc654fc57f2f91e2096ccfc868a
Iamasocial <klimbundin17@gmail.com>
2024-01-11 15:00:44 +0300

    12 lab

--- postgreslabs.sql
+++ postgreslabs.sql
@@ -1,4 +1,4 @@
--- Active: 1704800582835@@127.0.0.1@5432@test@db
+-- Active: 1704900343020@@127.0.0.1@5432@test@db
 CREATE DATABASE psqlLabs;
 
 CREATE SCHEMA IF NOT EXISTS db;
@@ -231,7 +231,7 @@ WHERE EXISTS(
 
 --6.2 Как бы вы решили предыдущую проблему, используя объединение?
 SELECT s.snum, s.sname FROM db.sellers s, db.clients c
-WHERE c.rating = 300 AND c.snum = s.snum;
+WHERE c.rating = 300 AND c.snum = s.snum;--// если речь идет о UNION, то никак :)
 
 
 --6.3 Напишите запрос, использующий оператор EXISTS, который выберет
@@ -286,3 +286,338 @@ WHERE s.snum != ALL(
 
 --6.8  Напишите запрос, который выбирал бы все заказы с суммой, больше, чем любая
 --(в обычном смысле) для заказчиков в Лондоне.
+SELECT onum, amt FROM db.orders o, db.clients c
+WHERE o.cnum = c.cnum AND c.city != 'London' AND amt > ANY(
+    SELECT amt FROM db.orders oo, db.clients cl
+    WHERE oo.cnum = cl.cnum AND cl.city = 'London'
+) ORDER BY onum;
+
+
+--6.9 Напишите предыдущий запрос с использованием MAX. // и MIN вероятно, иначе я хз как тогда делать
+SELECT onum, amt FROM db.orders o, db.clients c
+WHERE o.cnum = c.cnum AND c.city != 'London' AND
+amt < (
+    SELECT MAX(amt) FROM db.orders o, db.clients c
+    WHERE o.cnum = c.cnum AND c.city = 'London'
+) AND amt > (
+    SELECT MIN(amt) FROM db.orders o, db.clients c
+    WHERE o.cnum = c.cnum AND c.city = 'London'
+) ORDER BY onum;
+
+
+--7.1 Создайте объединение из двух запросов, которое показало бы имена,
+--города и оценки всех заказчиков. Те из них, которые имеют поле
+--rating=200 и более, должны, кроме того, иметь слова - "Высокий
+--Рейтинг", а остальные должны иметь слова "Низкий Рейтинг".
+SELECT cname, city, rating, 'Высокий рейтинг' FROM db.clients WHERE rating >= 200
+UNION
+SELECT cname, city, rating, 'Низкий рейтинг' FROM db.clients WHERE rating < 200;
+
+
+--7.2 Напишите команду, которая вывела бы имена и номера каждого продавца и
+--каждого заказчика, которые имеют больше чем один текущий
+--заказ. Результат представьте в алфавитном порядке.
+SELECT snum, sname FROM db.sellers s WHERE 1 < (
+    SELECT COUNT(onum) FROM db.orders o WHERE o.snum = s.snum 
+)
+UNION
+SELECT cnum, cname FROM db.clients c WHERE 1 < (
+    SELECT COUNT(onum) FROM db.orders o WHERE o.cnum = c.cnum
+)
+ORDER BY sname;
+
+
+--7.3 Сформируйте объединение из трех запросов. Первый выбирает поля
+--snum всех продавцов в San Jose; второй, поля cnum всех заказчиков в
+--San Jose; и третий - поля onum всех заказов на 3 октября.
+--Сохраните дубликаты между последними двумя запросами, но
+--устраните любую избыточность вывода между каждым из них и самым первым.
+SELECT snum FROM db.sellers WHERE city = 'San Jose'
+UNION
+(
+    SELECT cnum FROM db.clients WHERE city = 'San Jose'
+    UNION ALL
+    SELECT onum FROM db.orders WHERE odate = '1990-10-03'
+);
+
+--8.1 Напишите команду, которая поместила бы следующие значения в
+--указанном заказе в таблицу Продавцов:
+
+--city - San Jose,
+--name - Bianco,
+--comm - NULL,
+--snum - 1100.
+INSERT INTO db.sellers VALUES
+(1100, 'Bianco', 'San Jose', NULL);
+DELETE FROM db.sellers WHERE sname = 'Bianco';
+
+
+--8.2 Напишите команду, которая удалила бы все заказы заказчика Clemens из таблицы Заказов.
+DELETE FROM db.orders WHERE cnum = (
+    SELECT cnum FROM db.clients WHERE cname = 'Clemens'
+)
+
+
+--8.3 Напишите команду, которая увеличила бы оценку всех заказчиков в Риме на 100.
+UPDATE db.clients
+SET rating = rating + 100
+WHERE city = 'Rome';
+
+UPDATE db.clients
+SET rating = rating - 100
+WHERE city = 'Rome';
+
+
+--8.4 Продавец Serres оставил компанию. Переназначьте его заказчиков продавцу Motika.
+UPDATE db.clients
+SET snum = (
+    SELECT snum FROM db.sellers WHERE sname = 'Motika'
+) WHERE snum = (
+    SELECT snum FROM db.sellers WHERE sname = 'Serres'
+)
+
+--9.1 Предположите, что имеется таблица Multicust с такими
+--же именами столбцов, что и в таблице Продавцов. Напишите команду,
+--которая вставила бы всех продавцов (из таблицы Продавцов), имеющих 
+--более чем одного заказчика, в эту таблицу.
+CREATE TABLE db.multicust
+(
+    snum INTEGER PRIMARY KEY,
+    sname VARCHAR NOT NULL,
+    city VARCHAR NOT NULL,
+    comm DECIMAL
+);
+
+INSERT INTO db.multicust
+SELECT * FROM db.sellers WHERE snum IN(
+    SELECT snum FROM db.clients GROUP BY snum HAVING COUNT(cnum) > 1
+);
+SELECT * FROM db.multicust;
+DELETE FROM db.multicust WHERE 1=1;
+
+--9.2 Напишите команду, которая удаляла бы всех заказчиков не имеющих текущих заказов.
+DELETE FROM db.clients WHERE cnum NOT IN(
+    SELECT cnum FROM db.orders
+);
+
+
+--9.3 Напишите команду, которая увеличила бы на двадцать процентов комиссионные
+--всех продавцов, имеющих сумму текущих заказов выше $3,000.
+UPDATE db.sellers
+SET comm = comm + 0.2
+WHERE snum IN(
+    SELECT snum FROM db.orders GROUP BY snum HAVING SUM(amt) > 3000
+);
+
+SELECT * FROM db.sellers;
+
+UPDATE db.sellers
+SET comm = comm - 0.2
+WHERE snum IN(
+    SELECT snum FROM db.orders GROUP BY snum HAVING SUM(amt) > 3000
+);
+
+
+--10.1 Напишите предложение CREATE TABLE, которое вывело бы нашу таблицу Заказчиков.
+--почему это в 10 лабе, а не в нулевой?
+CREATE TABLE db.clients
+(
+    cnum INTEGER PRIMARY KEY,
+    cname VARCHAR NOT NULL,
+    city VARCHAR NOT NULL,
+    rating INTEGER NOT NULL,
+    snum INTEGER NOT NULL,
+    FOREIGN KEY(snum) REFERENCES db.sellers(snum)
+)
+
+
+--10.2 Напишите команду, которая давала бы возможность пользователю
+--быстро извлекать заказы, сгруппированные по датам, из таблицы Заказов.
+SELECT * FROM db.orders ORDER BY odate;
+
+--10.3 Если таблица Заказов уже создана, как вы можете заставить поле
+--onum быть уникальным (если допустить что все текущие значения уникальны)?
+ALTER TABLE db.orders ADD CONSTRAINT onum_unique UNIQUE (onum);
+
+
+--10.4 Создайте индекс, который разрешал бы каждому продавцу быстро
+--отыскивать его заказы, сгруппированные по датам.
+CREATE UNIQUE INDEX snum_index ON db.orders (snum);
+
+
+--10.5 Предположим, что каждый продавец имеет только одного заказчика 
+--с данной оценкой. Введите команду, которая его извлечет.
+SELECT cname
+FROM db.clients a
+WHERE cnum = (
+    SELECT cnum FROM db.clients b
+    WHERE b.rating = 100 AND b.snum = (
+        SELECT snum FROM db.sellers WHERE sname = 'Serres'
+    )
+)
+
+
+--11.1 Создайте таблицу Заказов так, чтобы все значения полей onum, а также
+--все комбинации полей cnum и snum, отличались друг от друга и чтобы
+--значения NULL исключались из поля даты.
+CREATE TABLE db.ordersV2
+(
+    onum INTEGER PRIMARY KEY,
+    amt DECIMAL,
+    odate DATE NOT NULL,
+    cnum INTEGER,
+    snum INTEGER,
+    FOREIGN KEY (cnum) REFERENCES db.clients(cnum),
+    FOREIGN KEY (snum) REFERENCES db.sellers(snum),
+    CONSTRAINT unique_cnum_and_snum UNIQUE (cnum, snum)
+);
+
+
+--11.2 Создайте таблицу Продавцов так, чтобы комиссионные по умолчанию
+--составляли 10%, не разрешались значения NULL, поле snum являлось первичным ключом
+--и чтобы все имена были в алфавитном порядке между A и M включительно
+--(учитывая, что все имена будут напечатаны в верхнем регистре).
+CREATE TABLE db.sellersV2
+(
+    snum INTEGER PRIMARY KEY,
+    sname VARCHAR NOT NULL CHECK (sname BETWEEN 'A' AND 'M'),
+    city VARCHAR NOT NULL,
+    comm DECIMAL DEFAULT .1
+);
+
+--11.3 Создайте таблицу Заказов учётом того, что поле onum
+--больше, чем поле cnum, а cnum больше, чем snum. Запрещены значения
+--NULL в любом из этих трех полей.
+CREATE TABLE db.ordersV3
+(
+    onum INTEGER PRIMARY KEY,
+    amt DECIMAL,
+    odate DATE NOT NULL,
+    cnum INTEGER NOT NULL CHECK (cnum < onum),
+    snum INTEGER NOT NULL CHECK (snum < cnum),
+    FOREIGN KEY (cnum) REFERENCES db.clients(cnum),
+    FOREIGN KEY (snum) REFERENCES db.sellers(snum),
+    CONSTRAINT unique_cnum_and_snum UNIQUE (cnum, snum)
+)
+
+--11.4 Создайте таблицу с именем Cityorders. Она должна содержать такие же
+--поля onum, amt и snum, что и таблица Заказов, и такие же поля
+--cnum и city, что и таблица Заказчиков, так что заказ каждого
+--заказчика будет вводиться в эту таблицу вместе с его городом.
+--Поле оnum будет первичным ключом Cityorders. Все поля в Cityorders
+--должны иметь ограничения при сравнении с таблицами Заказчиков и
+--Заказов. Допускается, что родительские ключи в этих таблицах уже
+--имеют соответствующие ограничения.
+CREATE TABLE db.cityorders (
+    onum INTEGER REFERENCES db.orders(onum) PRIMARY KEY,
+    amt DECIMAL REFERENCES db.orders(amt),
+    snum INTEGER REFERENCES db.orders(snum),
+    cnum INTEGER REFERENCES db.customers(cnum),
+    city VARCHAR REFERENCES db.customers(city)
+);
+
+
+--11.5 Усложним проблему. Переопределите таблицу Заказов следующим
+--образом: добавьте новый столбец с именем prev, который будет
+--идентифицирован для каждого заказа, поле onum предыдущего
+--заказа для этого текущего заказчика.
+--Выполните это с использованием внешнего ключа, ссылающегося на
+--саму таблицу Заказов.
+--Внешний ключ должен ссылаться также на поле cnum заказчика,
+--обеспечивающее определенную предписанную связь между текущим порядком и ссылаемым.
+ALTER TABLE db.orders
+ADD COLUMN prev INTEGER REFERENCES db.orders(onum);
+UPDATE db.orders o
+SET prev = (
+    SELECT MAX(onum) FROM db.orders o2 WHERE o2.cnum = o.cnum and o2.onum < 0.onum
+);
+
+
+--12.1 Создайте представление, которое показывало бы всех заказчиков, имеющих
+--самые высокие рейтинги.
+CREATE VIEW db.highRating AS
+    SELECT * FROM db.clients WHERE rating = (
+        SELECT MAX(rating) FROM db.clients
+    );
+SELECT * FROM db.highRating;
+DROP VIEW db.highRating;
+
+
+--12.2 Создайте представление, которое показывало бы номер продавца в каждом городе.
+CREATE VIEW db.sellerCity AS
+    SELECT city, snum FROM db.sellers;
+SELECT * FROM db.sellerCity;
+DROP VIEW db.sellerCity
+
+
+--12.3 Создайте представление, которое показывало бы усреднённый и общий заказы для каждого
+--продавца после его имени. Предполагается, что все имена уникальны.
+CREATE VIEW db.sellerStat AS
+    SELECT snum, ROUND(AVG(amt), 2), SUM(amt) FROM db.orders GROUP BY snum;
+SELECT * FROM db.sellerStat;
+DROP VIEw db.sellerStat;
+
+
+--12.4 Создайте представление, которое показывало бы каждого продавца с несколькими заказчиками.
+CREATE VIEW db.multiclient AS
+    SELECT snum FROM db.orders GROUP BY snum HAVING count(cnum) > 1;
+SELECT * FROM db.multiclient;
+DROP VIEW db.multiclient;
+
+
+--12.5 Какое из этих представлений - модифицируемое? // #2
+-- #1 CREATE VIEW Dailyorders
+--              AS SELECT DISTINCT cnum, snum, onum,
+--              odate
+--                FROM Orders;
+
+--        #2 CREATE VIEW Custotals
+--              AS SELECT cname, SUM (amt)
+--                 FROM Orders, Customers
+--                 WHERE Orders.cnum = customer.cnum
+--                 GROUP BY cname;
+
+--        #3 CREATE VIEW Thirdorders
+--              AS SELECT *
+--                 FROM Dailyorders
+--                 WHERE odate = 10/03/1990;
+
+--        #4 CREATE VIEW Nullcities
+--              AS SELECT snum, sname, city
+--                 FROM Salespeople
+--                 WHERE city IS NULL
+--                    OR sname BETWEEN 'A' AND 'MZ';
+
+
+-- 12.6 Создайте представление таблицы Продавцов с именем Commissions
+--    (Комиссионные). Это представление должно включать только поля
+--    comm и snum. С помощью этого представления можно будет вводить
+--    или изменять комиссионные, но только для значений между .10 и .20.
+CREATE VIEW db.Commissions AS
+    SELECT comm, snum FROM db.sellers;
+SELECT * FROM db.commissions;
+DROP VIEW db.commissions;
+
+
+--12.7 Некоторые SQL-реализации имеют встроенную константу, представляющую текущую дату,
+--    иногда называемую " CURDATE ". 
+--    Слово CURDATE может, следовательно, использоваться в операторе SQL
+--    и заменяться текущей датой, когда его значение станет доступным, с помощью таких
+--    команд как SELECT или INSERT. Мы будем использовать
+--    представление таблицы Заказов с именем Entryorders для вставки строк
+--    в таблицу Заказов. Создайте таблицу заказов так, чтобы CURDATE
+--    автоматически вставлялась в поле odate, если не указано другое значение.
+--    Затем создайте представление Entryorders так, чтобы значения не могли быть указаны.
+CREATE TABLE db.entryorders
+(
+    onum INTEGER PRIMARY KEY,
+    amt DECIMAL,
+    odate DATE NOT NULL DEFAULT current_date,
+    cnum INTEGER NOT NULL CHECK (cnum < onum),
+    snum INTEGER NOT NULL CHECK (snum < cnum),
+    FOREIGN KEY (cnum) REFERENCES db.clients(cnum),
+    FOREIGN KEY (snum) REFERENCES db.sellers(snum),
+    CONSTRAINT unique_cnum_and_snum UNIQUE (cnum, snum)
+);
+SELECT * FROM db.entryorders;
+DROP TABLE db.entryorders;
